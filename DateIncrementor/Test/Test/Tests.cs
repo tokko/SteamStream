fce1eff2c1c914a3e314ce039237b65f084b6521
tokko <andreg@kth.se>
2017-03-15 20:02:09 +0100

    initial commit

---
+++ DateIncrementor/Test/Test/Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Weekly()
+        {
+            var incrementor = new DateIncrementor.DateIncrementor();
+
+            incrementor.WithStartDate(DateTime.Parse("2017-02-14")).OnDayOfWeek(2).WithWeekIncrement(1);
+
+            var res = incrementor.GetDates().Take(4).ToList();
+
+            Assert.NotNull(res);
+            Assert.That(res.Count, Is.EqualTo(4));
+            Assert.That(res.First().Year, Is.EqualTo(2017));
+            Assert.That(res.First().Month, Is.EqualTo(2));
+            Assert.That(res.First().Day, Is.EqualTo(14));
+        }
+
+        [Test]
+        public void Monthly()
+        {
+            var incrementor = new DateIncrementor.DateIncrementor();
+
+            incrementor.WithStartDate(DateTime.Parse("2017-02-14")).WithMonthIncrement(1);
+
+            var res = incrementor.GetDates().Take(4).ToList();
+
+            Assert.NotNull(res);
+            Assert.That(res.Count, Is.EqualTo(4));
+
+            for (var i = 2; i <= 4; i++)
+            {
+                var dateTime = res[i - 2];
+                Assert.That(dateTime.Year, Is.EqualTo(2017));
+                Assert.That(dateTime.Month, Is.EqualTo(i));
+                Assert.That(dateTime.Day, Is.EqualTo(14));
+            }
+
+        }
+
+        [Test]
+        public void Yearly()
+        {
+            var incrementor = new DateIncrementor.DateIncrementor();
+
+            incrementor.WithStartDate(DateTime.Parse("2017-02-14")).WithYearIncrement(1).OnMonth(2).OnDay(24);
+
+            var res = incrementor.GetDates().Take(4).ToList();
+
+            Assert.NotNull(res);
+            Assert.That(res.Count, Is.EqualTo(4));
+
+            for (var i = 2; i <= 4; i++)
+            {
+                var dateTime = res[i - 2];
+                Assert.That(dateTime.Year, Is.EqualTo(2017+i-2));
+                Assert.That(dateTime.Month, Is.EqualTo(2));
+                Assert.That(dateTime.Day, Is.EqualTo(24));
+            }
+
+        }
+    }
+}
---
+++ DateIncrementor/src/DateIncrementor/DateIncrementor.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DateIncrementor
+{
+    public class DateIncrementor
+    {
+        private DateTime _startDate;
+        private DateTime? _endDate;
+        private int _yearIncrement;
+        private int _monthIncrement;
+        private int _weekIncrement;
+        private int? _weekDay;
+        private bool _onLastDayOfMonth;
+        private int? _day;
+        private int? _month;
+
+        public DateIncrementor WithStartDate(DateTime startDate)
+        {
+            _startDate = startDate;
+            return this;
+        }
+
+        public DateIncrementor WithEndDate(DateTime endDate)
+        {
+            _endDate = endDate;
+            return this;
+        }
+
+        public DateIncrementor WithYearIncrement(int yearIncrement)
+        {
+            _yearIncrement = yearIncrement;
+            return this;
+        }
+
+        public DateIncrementor WithMonthIncrement(int monthIncrement)
+        {
+            _monthIncrement = monthIncrement;
+            return this;
+        }
+
+        public DateIncrementor WithWeekIncrement(int weekIncrement)
+        {
+            _weekIncrement = weekIncrement;
+            return this;
+        }
+
+        public DateIncrementor OnDayOfWeek(int weekDay)
+        {
+            _weekDay = weekDay + 1;
+            return this;
+        }
+
+        public DateIncrementor OnMonth(int month)
+        {
+            _month = month;
+            return this;
+        }
+
+        public DateIncrementor OnDay(int day)
+        {
+            _day = day;
+            return this;
+        }
+        public DateIncrementor OnLastDayOfMonth(bool onLastDayOfMonth)
+        {
+            _onLastDayOfMonth = onLastDayOfMonth;
+            return this;
+        }
+
+        public IEnumerable<DateTime> GetDates()
+        {
+            var date = _startDate;
+            date = AdjustDate(date);
+            if (date >= _startDate)
+                yield return date;
+            while (date <= (_endDate ?? DateTime.MaxValue))
+            {
+                date = date.AddYears(_yearIncrement);
+                date = date.AddMonths(_monthIncrement);
+                date = date.AddDays(7 * _weekIncrement);
+                date = AdjustDate(date);
+                yield return date;
+            }
+        }
+
+        private DateTime AdjustDate(DateTime date)
+        {
+            if (_onLastDayOfMonth)
+                for (; date.AddDays(1).Month < date.Month; date = date.AddDays(1)) ;
+            if (_weekDay.HasValue)
+            {
+                for (; date.AddDays(-1).DayOfWeek > (DayOfWeek) _weekDay.Value; date = date.AddDays(-1)) ;
+                for (; ((int) date.AddDays(1).DayOfWeek) < _weekDay.Value; date = date.AddDays(-1)) ;
+            }
+            if (_month.HasValue)
+            {
+                for (; date.AddMonths(-1).Month > _month.Value; date = date.AddMonths(-1)) ;
+                for (; date.AddMonths(1).Month < _month.Value; date = date.AddMonths(1)) ;
+            }
+            if (_day.HasValue)
+            {
+                for (; date.AddDays(-1).Day > _day.Value || date.AddDays(1).Month > date.Month; date = date.AddDays(-1)) ;
+                for (; date.AddDays(1).Day <= _day.Value || date.AddDays(1).Month > date.Month; date = date.AddDays(1)) ;
+            }
+            return date;
+        }
+    }
+}
